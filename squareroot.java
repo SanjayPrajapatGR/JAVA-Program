72c876f3a6e4c0e29519c3c94be776bbe3cfc393
ravipatel73842 <41752089+ravipatel73842@users.noreply.github.com>
2018-08-16 22:04:34 -0700

    find the square root using math function

---
+++ squareroot.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+class square
+{
+	public static void main(String [] args)
+	{
+		Scanner n=new Scanner(System.in);
+		double a=0;
+		double m=n.nextDouble();
+		a=Math.sqrt(m);
+		System.out.println("Square Root is :"+a);
+	}
+}
