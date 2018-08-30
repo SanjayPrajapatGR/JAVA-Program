f521cb4793479d0404c3d8ada982281973f6a7d2
ravipatel73842 <41752089+ravipatel73842@users.noreply.github.com>
2018-08-30 13:06:49 +0530

    addition of two number using method overloding

---
+++ addition_of_two_number_using_method_overloding.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class addition
+{
+	int x,y;
+	float q,w;
+	void add(int a,int b)
+	{
+		x=a;
+		y=b;
+		int z=x+y;
+		System.out.println("Addition of two integer number is:"+z);
+	}
+	void add(float a,float b)
+	{
+		q=a;
+		w=b;
+		float e=q+w;
+		System.out.println("Addition of two integer number is:"+e);
+	}
+}
+class addition_of_two_number_using_method_overloding
+{
+	public static void main(String [] args)
+	{
+		addition m=new addition();
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the integer number");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		m.add(a,b);
+		
+		System.out.println("enter the float number");
+		float c=s.nextFloat();
+		float d=s.nextFloat();
+		m.add(c,d);
+	}
+}
+		
