9aeaafd187cb6213b4d66fbe2900e563c34f99b5
ravipatel73842 <41752089+ravipatel73842@users.noreply.github.com>
2018-09-13 09:10:50 -0700

    sort the array using merge sort

---
+++ mergeSort.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+class mergeSort
+{  
+    static int _mergeSort(int arr[], int temp[], int left, int right)
+    {
+      int mid, inv_count = 0;
+      if (right > left)
+      {
+        mid = (right + left)/2;
+        inv_count  = _mergeSort(arr, temp, left, mid);
+        inv_count += _mergeSort(arr, temp, mid+1, right);
+       inv_count += merge(arr, temp, left, mid+1, right);
+      }
+      return inv_count;
+    }
+    static int merge(int arr[], int temp[], int left, int mid, int right)
+    {
+      int i, j, k;
+      int inv_count = 0;
+      
+      i = left; /* i is index for left subarray*/
+      j = mid;  /* j is index for right subarray*/
+      k = left; /* k is index for resultant merged subarray*/
+      while ((i <= mid - 1) && (j <= right))
+      {
+        if (arr[i] <= arr[j])
+        {
+          temp[k++] = arr[i++];
+        }
+        else
+        {
+          temp[k++] = arr[j++];
+          inv_count = inv_count + (mid - i);
+        }
+      }
+      while (i <= mid - 1)
+        temp[k++] = arr[i++];
+      while (j <= right)
+        temp[k++] = arr[j++];
+      for (i=left; i <= right; i++)
+        arr[i] = temp[i];  
+      return inv_count;
+    }
+  
+     public static void main(String[] args) 
+    {
+	Scanner s=new Scanner(System.in);
+        System.out.println("enter the array size");
+	int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+		int temp[] = new int[n];
+        int count=_mergeSort(arr, temp, 0, n - 1);
+        System.out.println("Number of inversions are "+count );
+     
+    }
+}
